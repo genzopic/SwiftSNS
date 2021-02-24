01c9f683abfa5c4eb5701dc5180c63ee78100dba
genzo <genzo.pic@gmail.com>
2021-02-24 11:27:58 +0900

    Initial Commit

---
+++ SwiftSNS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftSNS
+//
+//  Created by yasuyoshi on 2021/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
